fd9f20a86ac6c4c1652b3fd1cd91df6b64a5dad0
sauqi <rizasauqi28@gmail.com>
2022-10-27 22:43:46 +0700

    test doang

--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -11,4 +11,5 @@ class Checkout extends Model
     use HasFactory, SoftDeletes;
     
     protected $fillable = ['user_id', 'camp_id', 'card_number', 'expired', 'cvc', 'is_paid'];
+    // protected $fillable = ['user_id', 'camp_id', 'card_number', 'expired', 'cvc', 'is_paid'];
 }
