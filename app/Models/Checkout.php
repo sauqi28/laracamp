a4be95a483ff263b0d8d5073f0d0682874b3acfc
sauqi <rizasauqi28@gmail.com>
2022-10-27 22:45:01 +0700

    finish migration camp, camp_benefit, checkout

--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -11,5 +11,4 @@ class Checkout extends Model
     use HasFactory, SoftDeletes;
     
     protected $fillable = ['user_id', 'camp_id', 'card_number', 'expired', 'cvc', 'is_paid'];
-    // protected $fillable = ['user_id', 'camp_id', 'card_number', 'expired', 'cvc', 'is_paid'];
-}
+   }
